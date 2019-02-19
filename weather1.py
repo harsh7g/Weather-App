7dc7fe00fb3dd38eb2a87a3e83de65848bc0129a
iamharshgupta <42388898+iamharshgupta@users.noreply.github.com>
2019-02-19 14:05:00 +0530

    Change units

--- weather1.py
+++ weather1.py
@@ -8,17 +8,18 @@ x = response.json()
 if x["cod"] != "404": 
 	y = x["main"] 
 	current_temperature = y["temp"] 
+	current_temperature=current_temperature-273.0
 	current_pressure = y["pressure"] 
 	current_humidiy = y["humidity"] 
 	z = x["weather"] 
 	weather_description = z[0]["description"] 
 	curr_time = x["sys"]
-	print(" Temperature (in kelvin unit) = " +
+	print(" Temperature (in Celcius unit) = " +
 					str(current_temperature) +
 		"\n atmospheric pressure (in hPa unit) = " +
 					str(current_pressure) +
 		"\n humidity = " +
-					str(current_humidiy) +
+					str(current_humidiy) + " %"
 		"\n description = " +
 					str(weather_description))
 else: 
